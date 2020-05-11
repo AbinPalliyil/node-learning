e50896c5d53110c61464c162b8bd3c41f7d98213
Abin <abin@luminescent.digital>
2020-05-11 13:29:22 +0530

    Middleware working example

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+	console.log('First middleware');
+	next();
+});
+
+app.use((req, res, next) => {
+	console.log('Second middleware');
+	res.send('Middleware example');
+});
+
 app.listen(5000, () => {
-    console.log("App is running in port 5000")
-})
-
-
-
-
+	console.log('App is running in port 5000');
+});
