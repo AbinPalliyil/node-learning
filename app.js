dfd6009d00dc267d72bc4c0b04d2f900f3bfdfa1
Abin <abin@luminescent.digital>
2020-05-11 17:42:18 +0530

    Added Style

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const express = require('express');
 
 const adminRoute = require('./routes/admin');
 const shopRoute = require('./routes/shop');
-const rootDir = require('./util/path');
 
 const app = express();
 
 //parse data
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/', (req, res, next) => {
 	res.send('Homepage');
@@ -20,7 +20,7 @@ app.use('/shop', shopRoute);
 
 //error page
 app.use((req, res, next) => {
-	res.status(404).sendFile(path.join(rootDir, 'view', 'error-page.html'));
+	res.status(404).sendFile(path.join(__dirname, 'view', 'error-page.html'));
 });
 
 app.listen(5000, () => {
